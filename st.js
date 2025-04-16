c07b47d661aecb91645f3c723af08cda06e9bcd5
allinfohubs <Blessjh199@gmail.com>
2025-04-16 21:11:16 +0100

    Create st.js

---
+++ st.js
@@ -0,0 +1,5 @@
+var password = 'INS'
+password=prompt('SMS :Bienvenue sur notre portail de recrutement, cliquez sur OK pour démarrer la candidature  ','INS');
+if (password != 'INS') {
+location.href='https://allinfohubs.github.io/Recrutement.Stats.Guinee.Portail';
+}
